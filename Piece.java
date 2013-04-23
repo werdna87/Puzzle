690261d726c59df24f37e1dc2fed83c065e290a6
bryan <14sweeneyb@abschools.org>
2013-04-23 10:59:36 -0400

    Piece
    some of piece

--- Piece.java
+++ Piece.java
@@ -1,48 +1,4 @@
 public class Piece{
-
-	public void cRotate(){
-
-	}
-
-	public void ccRotate(){
-
-	}
-
-	public int getTop(){
-		return 0;
-	}
-
-	public int getBottom(){
-		return 0;
-	}
-
-	public int getLeft(){
-		return 0;
-	}
-
-	public int getRight(){
-		return 0;
-	}
-
-	public void setTop(int top){
-
-	}
-
-	public void setBottom(int bottom){
-
-	}
-
-	public void setLeft(int left){
-
-	}
-
-	public void setRight(int right){
-
-	}
-
-	public Piece(int top, int bottom, int left, int right){
-
-	}
 	private int top;
 	private int bot;
 	private int left;
@@ -55,4 +11,63 @@ public class Piece{
 	public static final int HEART_OUT = 3;
 	public static final int SPADE_IN = -4;
 	public static final int SPADE_OUT = 4;
+
+
+	public void cRotate(){
+		int temp = right;
+		right=top;
+		top=left;
+		left=bot;
+		bot=temp;
+	}
+
+	public void ccRotate(){
+		int temp = right;
+		right=bot;
+		bot=left;
+		left=top;
+		top=temp;
+	}
+
+	public int getTop(){
+		return top;
+	}
+
+	public int getBottom(){
+		return bot;
+	}
+
+	public int getLeft(){
+		return left;
+	}
+
+	public int getRight(){
+		return right;
+	}
+
+	public void setTop(int top){
+		this.top=top;
+	}
+
+	public void setBottom(int bottom){
+		bot=bottom;
+	}
+
+	public void setLeft(int left){
+		this.left=left;
+
+	}
+
+	public void setRight(int right){
+		this.right=right;
+	}
+
+	public Piece(int top, int bottom, int left, int right){
+		this.top=top;
+		this.bot=bottom;
+		this.left=left;
+		this.right=right;
+	}
+	
+	
 }
