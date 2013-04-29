e69aee4e8f269b5f001d02639dd441f448b4013a
Andrew Borghesani <coolkid999@gmail.com>
2013-04-29 11:31:53 -0400

    bleh

--- Solver.java
+++ Solver.java
@@ -16,8 +16,15 @@ public class Solver{
 			}
 			piece.cRotate();
 		}
-		if(b.doesFit(piece,p)){
-			
+		if(bool=true){
+			if(p.getX()<= b.getRows()){
+				solve(b,new Point(p.x+1,p.y),i+1);
+			}
+			else{
+				p.x=0;
+				p.y=p.y+1;
+				solve(b,p,i+1);
+			}
 		}
 	}
 }
