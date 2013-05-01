e0232a5c471df5bc162d1d6af829de9e7e9f1fc9
bryan <14sweeneyb@abschools.org>
2013-05-01 11:41:20 -0400

    solver

--- Solver.java
+++ Solver.java
@@ -1,23 +1,28 @@
 import java.awt.Point;
 
 public class Solver{
-
+	private Piece[] used = Board.PIECES;
+	
 	public void solve(Board b){
 		solve(b,new Point(0,0),0);
 	}
 	public void solve(Board b, Point p,int i){
+		if(p.y>b.getRows()){
+			return;
+		}
 		Boolean bool;
-		Piece piece = Board.PIECES[i];
+		Piece piece = used[i];
 		for(int j=0;j<4;j++){
 			if(b.doesFit(piece,p)){
 				b.setPiece(piece,p);
 				bool=true;
+				used[i]=null;
 				break;
 			}
 			piece.cRotate();
 		}
 		if(bool=true){
-			if(p.getX()<= b.getRows()){
+			if(p.getX()<= b.getCols()){
 				solve(b,new Point(p.x+1,p.y),i+1);
 			}
 			else{
@@ -25,6 +30,21 @@ public class Solver{
 				p.y=p.y+1;
 				solve(b,p,i+1);
 			}
+		}else{
+			if(p.getX()-1>=0){
+				solve(b,new Point(p.x-1,p.y),i+1);
+				}
+			else{
+				p.x=b.getCols()-1;
+				p.y=p.y-1;
+			}
 		}
+		used[i]=piece;
+	}
+	
+	public static void main(String[]args){
+		Solver solve = new Solver();
+		Board b = new Board(3,3);
+		solve.solve(b);
 	}
 }
