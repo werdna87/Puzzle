a1b70c081fa33a31016df048574620666684280d
bryan <14sweeneyb@abschools.org>
2013-05-09 11:27:07 -0400

    working on it

--- Solver.java
+++ Solver.java
@@ -1,28 +1,36 @@
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class Solver{
-	private Piece[] used = Board.PIECES;
+	private ArrayList<Piece> used = new ArrayList<Piece>();
 	
 	public void solve(Board b){
+		for(Piece p:Board.PIECES){
+			used.add(p);
+		}
 		solve(b,new Point(0,0),0);
 	}
 	public void solve(Board b, Point p,int i){
+//		if(i>=used.size()){
+		if(p.x>=b.getCols()||p.y>=b.getRows()){
+			i=0;
+		}
 		if(p.y>b.getRows()){
 			return;
 		}
 		Boolean bool;
-		Piece piece = used[i];
+		Piece piece = used.get(i);
 		for(int j=0;j<4;j++){
 			if(b.doesFit(piece,p)){
 				b.setPiece(piece,p);
 				bool=true;
-				used[i]=null;
+				used.set(i,null);
 				break;
 			}
 			piece.cRotate();
 		}
 		if(bool=true){
-			if(p.getX()<= b.getCols()){
+			if(p.getX()> b.getCols()-1){
 				solve(b,new Point(p.x+1,p.y),i+1);
 			}
 			else{
@@ -39,7 +47,7 @@ public class Solver{
 				p.y=p.y-1;
 			}
 		}
-		used[i]=piece;
+		used.set(i,piece);
 	}
 	
 	public static void main(String[]args){
